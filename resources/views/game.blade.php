5186992b7dfdc613b400d9e58bc13e873085569e
Mohamed Yousef <modi401@hotmail.com>
2021-01-06 08:59:53 +0200

    Update game.blade.php

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="row my-5">
+    <div class="row mb-5">
         <div class="col-md-6 text-right">
             <img src="{{ asset('images/card_back.jpg') }}" id="draw-card" />
         </div>
@@ -11,21 +11,13 @@
     </div>
     <p class="text-center">Click the card to Draw!</p>
 
-    <div class="row my-5">
+    <div class="row my-1">
         <div class="col-md-4 offset-md-4 text-center">
             <meta name="csrf-token" content="{{ csrf_token() }}" />
             <input type="hidden" name="suit" value="{{ $suit }}">
             <input type="hidden" name="value" value="{{ $value }}">
-            <div class="alert alert-info">
-                Chance: <span id="chance">0</span>%
-            </div>
-        </div>
-    </div>
-    
-    <div class="row my-5">
-        <div class="col-md-4 offset-md-4 text-center">
-            <div class="alert alert-success d-none" id="win">
-                <h4></h4>
+            <div class="alert alert-info" id="chance">
+                <!-- chance % -->
             </div>
         </div>
     </div>
